8907815171575ec6a13970407afabfbab52530d7
flpsno <flpsno@hotmail.com>
2017-08-11 19:54:05 -0300

    Aula02 concluída.

---
+++ Aula002/Caneta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula002
+{
+    public class Caneta
+    {
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public double Ponta { get; set; }
+        public int Carga { get; set; }
+        public Boolean Tampada { get; set; }
+
+        public void Status()
+        {
+            Console.WriteLine("Modelo: {0}", Modelo);
+            Console.WriteLine("Uma caneta {0} ", Cor);
+            Console.WriteLine("Ponta: {0}", Ponta);
+            Console.WriteLine("Carga: {0}", Carga);
+            Console.WriteLine("Está tampada? {0}", Tampada ? "Sim" : "Não");
+        }
+
+        public void Rabiscar()
+        {
+            if (Tampada) {
+                Console.WriteLine("Erro, não posso rabiscar");
+            } else
+            {
+                Console.WriteLine("Estou rabiscando");
+            }
+        }
+
+        public void Tampar()
+        {
+            Tampada = true;
+        }
+
+        public void Destampar()
+        {
+            Tampada = false;
+        }
+
+    }
+}
--- Aula002/Program.cs
+++ Aula002/Program.cs
@@ -10,6 +10,24 @@ namespace Aula002
     {
         static void Main(string[] args)
         {
+            var c1 = new Caneta();
+            c1.Cor = "Azul";
+            c1.Ponta = 0.5F;
+            c1.Tampar();           
+            c1.Status();
+            c1.Rabiscar();
+            
+            Console.WriteLine("--------------------------------");
+
+            var c2 = new Caneta();
+            c2.Modelo = "Hostnet";
+            c2.Cor = "Preta";
+            c2.Destampar();
+            c2.Status();
+            c2.Rabiscar();
+
+            Console.ReadLine();
+
         }
     }
 }
