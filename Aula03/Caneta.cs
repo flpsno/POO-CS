6c4e88bb32ea939bfa41eaa6b231ca92325f7a24
flpsno <flpsno@hotmail.com>
2017-08-20 18:44:00 -0300

    Adicionada aula03

---
+++ Aula03/Caneta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula002
+{
+    public class Caneta
+    {
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        private double Ponta { get; set; }
+        protected int Carga { get; set; }
+        protected Boolean Tampada { get; set; }
+
+        public void Status()
+        {
+            Console.WriteLine("Modelo: {0}", Modelo);
+            Console.WriteLine("Uma caneta {0} ", Cor);
+            Console.WriteLine("Ponta: {0}", Ponta);
+            Console.WriteLine("Carga: {0}", Carga);
+            Console.WriteLine("Está tampada? {0}", Tampada ? "Sim" : "Não");
+        }
+
+        public void Rabiscar()
+        {
+            if (Tampada) {
+                Console.WriteLine("Erro, não posso rabiscar");
+            } else
+            {
+                Console.WriteLine("Estou rabiscando");
+            }
+        }
+
+        protected void Tampar()
+        {
+            Tampada = true;
+        }
+
+        protected void Destampar()
+        {
+            Tampada = false;
+        }
+
+    }
+}
---
+++ Aula03/Program.cs
@@ -0,0 +1,26 @@
+﻿using Aula002;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var c1 = new Caneta();
+            c1.Modelo = "Bic cristal";
+            c1.Cor = "Azul";
+            //c1.Ponta = 1.5;
+            //c1.Carga = 1;
+            c1.Status();
+
+            Console.ReadLine();
+
+        }
+    }
+}
